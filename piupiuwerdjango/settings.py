031f702923704f3158a09af05d77a312180b51e3
Mariana Serrão Guilherme <mserraoguilherme@gmail.com>
2020-07-14 21:21:03 -0300

    fix database misspell

--- piupiuwerdjango/settings.py
+++ piupiuwerdjango/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'piupiuwerdjango.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'digitaldb',
+        'NAME': 'digital',
         'USER': 'mari',
         'PASSWORD': 'polijunior',
         'HOST': 'localhost',

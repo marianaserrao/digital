9fe7a5502e22d5ed63a3505dfa5b0193a108c630
Mariana Serrão Guilherme <mserraoguilherme@gmail.com>
2020-07-14 21:14:33 -0300

    change database to production

--- piupiuwerdjango/settings.py
+++ piupiuwerdjango/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'digitaldb',
-        'USER': 'maridb',
+        'USER': 'mari',
         'PASSWORD': 'polijunior',
         'HOST': 'localhost',
         'PORT': '',
